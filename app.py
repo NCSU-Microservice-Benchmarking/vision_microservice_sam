71c519d43272ceccbabb9bde0e8a10bb14d72308
Jordan R <jlrobe25@ncsu.edu>
2023-10-31 01:10:51 +0000

    made chages to app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ model.info()
 
 # initial the model
 # model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt', source='local')
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-model.eval()
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+# model.eval()
 
 @app.route('/greet')
 def greet():
@@ -44,19 +44,23 @@ def detect():
     # # convert the image into a opencv image
     # image = cv2.imdecode(image, -1)
     # send the image to the model and get the result
+    # Display model information (optional)
+    model.info()
+
     # Run inference
     model('path/to/image.jpg')
+    
     # get the bounding boxes of the detected objects
-    boxes = results.xyxy[0].cpu().numpy()
-    #model('ultralytics/assets/zidane.jpg', bboxes=[439, 437, 524, 709])
+    # boxes = results.xyxy[0].cpu().numpy()
+    # model('ultralytics/assets/zidane.jpg', bboxes=[439, 437, 524, 709])
     # draw the bounding boxes on the image
-    for box in boxes:
-        x1,y1,x2,y2 = int(box[0]),int(box[1]),int(box[2]),int(box[3])
-        image = cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),2)
+    # for box in boxes:
+        # x1,y1,x2,y2 = int(box[0]),int(box[1]),int(box[2]),int(box[3])
+        # image = cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),2)
         # put the confidence value and category name on the image
-        image = cv2.putText(image,str(box[4]),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        # image = cv2.putText(image,str(box[4]),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
     # convert image to a binary string
-    image = cv2.imencode('.png', image)[1].tostring()
+    # image = cv2.imencode('.png', image)[1].tostring()
     # send the image to the client as an png image encoded as a binary string
     return Response(image, mimetype='image/png')
 
