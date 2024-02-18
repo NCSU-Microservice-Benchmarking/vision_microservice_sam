7a031e113db77a55e5e97cef380b8a2c00f36dd5
Jordan Robertson <jlrobe25@ncsu.edu>
2024-02-18 16:12:12 -0500

    changed from SAM to YOLOv8 (not tested)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from ultralytics import SAM
+from ultralytics import YOLO
 import torch
 import time
 import cv2
@@ -12,11 +12,11 @@ import requests
 app = Flask(__name__)
 
 # Load a model
-model = SAM('sam_b.pt')
+model = YOLO('yolov8n-seg.pt')
 
 # initial the model
 # model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt', source='local')
-# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('ultralytics/yolov8', 'yolov8s')
 # model.eval()
 
 @app.route('/greet')
@@ -38,27 +38,29 @@ def detect():
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # cv2.IMREAD_COLOR in OpenCV 3.1
     # image = np.frombuffer(image_buffer, np.uint8)
     # print(image.shape)
-    # # image = request.files.get('image')
+    # image = request.files.get('image')
     # # # convert image string to a numpy array
     # # image = np.fromstring(image, np.uint8)
     # # convert the image into a opencv image
     # image = cv2.imdecode(image, -1)
     # send the image to the model and get the result
     # Display model information (optional)
-    # Run inference
+    # Run inference 
     results = model(image)
-    for r in results:
-        image = r.plot()  # plot a BGR numpy array of predictions
+    # for r in results:
+        # image = r.plot()  # plot a BGR numpy array of predictions
     
     # get the bounding boxes of the detected objects
-    # boxes = results.xyxy[0].cpu().numpy()
+    boxes = results.xyxy[0].cpu().numpy()
     # model('ultralytics/assets/zidane.jpg', bboxes=[439, 437, 524, 709])
     # draw the bounding boxes on the image
-    # for box in boxes:
-        # x1,y1,x2,y2 = int(box[0]),int(box[1]),int(box[2]),int(box[3])
-        # image = cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),2)
+    for box in boxes:
+        x1,y1,x2,y2 = int(box[0]),int(box[1]),int(box[2]),int(box[3])
+        image = cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),2)
         # put the confidence value and category name on the image
-        # image = cv2.putText(image,str(box[4]),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        image = cv2.putText(image,str(box[4]),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+    # save the image to local as "result.png"
+    cv2.imwrite("result.png", image)
     # convert image to a binary string
     image = cv2.imencode('.png', image)[1].tostring()
     # send the image to the client as an png image encoded as a binary string
